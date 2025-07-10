e849c21950ad8b4e4f20138d090ce95eb803e274
Matthew Davoren <matthew.davoren@ventura.org>
2025-07-10 00:04:05 +0000

    styled sidebar update and added slideshow to main content

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,18 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+document.addEventListener("turbo:load", () => {
+  const slides = document.querySelectorAll(".slide");
+  console.log("Slides found:", slides.length);
+
+  let current = 0;
+
+  if (slides.length > 1) {
+    setInterval(() => {
+      slides[current].classList.remove("active");
+      current = (current + 1) % slides.length;
+      slides[current].classList.add("active");
+    }, 5000); // 60 seconds
+  }
+});
