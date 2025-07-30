cc4e76ca507808d869358492233dedfc815d448c
Matthew Davoren <matthew.davoren@ventura.org>
2025-07-30 20:27:21 +0000

    Probation Transfer form fixed and rendering

--- app/controllers/probation_transfer_requests_controller.rb
+++ app/controllers/probation_transfer_requests_controller.rb
@@ -18,91 +18,24 @@ class ProbationTransferRequestsController < ApplicationController
   end
 
   def new
-  @probation_transfer_request = ProbationTransferRequest.new
-
-  employee_id = session[:user]["employee_id"]
-  @employee = Employee.find_by(EmployeeID: employee_id)
-
-  unit_code = @employee&.[]("Unit")
-  unit = Unit.find_by(unit_id: unit_code)
-
-  department = Department.find_by(department_id: unit&.[]("department_id"))
-  division   = Division.find_by(division_id: department&.[]("division_id"))
-  agency     = Agency.find_by(agency_id: division&.[]("agency_id"))
-
-  @prefill_data = {
-    employee_id: @employee&.[]("EmployeeID"),
-    name: "#{@employee&.[]("First_Name")} #{@employee&.[]("Last_Name")}",
-    phone: @employee&.[]("Work_Phone"),
-    email: @employee&.[]("EE_Email"),
-    agency: agency&.agency_id,
-    division: division&.division_id,
-    department: department&.department_id,
-    unit: unit ? "#{unit.unit_id} - #{unit.long_name}" : nil
-  }
-
-  @agency_options = Agency.all.map { |a| [a.long_name, a.agency_id] }
-
-  @division_options = if agency
-    Division.where(agency_id: agency.agency_id).map { |d| [d.long_name, d.division_id] }
-  else
-    []
-  end
-
-  @department_options = if division
-    Department.where(division_id: division.division_id).map { |d| [d.long_name, d.department_id] }
-  else
-    []
-  end
-
-  @unit_options = if department
-    Unit.where(department_id: department.department_id).map { |u| ["#{u.unit_id} - #{u.short_name}", u.unit_id] }
-  else
-    []
-  end
-
-  @form_logo = "/assets/images/default-logo.svg"
-
-  @form_pages = [
-    {
-      title: "Employee Info",
-      fields: [
-        { name: "employee_id", label: "Employee ID", type: "text", required: true },
-        { name: "name", label: "Name", type: "text", required: true },
-        { name: "email", label: "Email", type: "text", required: true },
-        { name: "phone", label: "Phone", type: "text", required: true }
-      ]
-    },
-    {
-      title: "Agency Info"
-    },
-    {
-      title: "Transfer Request Details",
-      fields: [
-        { name: "work_location", label: "Work Location", type: "text", required: true },
-        { name: "current_assignment_date", label: "Current Assignment Date", type: "text", required: true },
-        { name: "desired_transfer_destination", label: "Desired Transfer Destination", type: "multi-select", options: ["East County", "West County", "Downtown", "Remote", "Other"] }
-      ]
-    }
-  ]
-end
-
-  def edit
+    @probation_transfer_request = ProbationTransferRequest.new
+    prepare_new_transfer_form
   end
 
   def create
-    raw_params = probation_transfer_request_params
-
-    @probation_transfer_request = ProbationTransferRequest.new(raw_params)
+    @probation_transfer_request = ProbationTransferRequest.new(probation_transfer_request_params)
     @probation_transfer_request.status = 0
 
     if @probation_transfer_request.save
       TransferMailer.notify(@probation_transfer_request).deliver_later
       redirect_to probation_transfer_requests_path, notice: "Transfer request submitted!"
     else
+      prepare_new_transfer_form
       render :new
     end
   end
+
+  def edit; end
 
   def update
     if @probation_transfer_request.update(probation_transfer_request_params)
@@ -138,5 +71,73 @@ end
       :desired_transfer_destination,
       :status
     )
+  end
+
+  def prepare_new_transfer_form
+    employee_id = session[:user]["employee_id"]
+    @employee = Employee.find_by(EmployeeID: employee_id)
+
+    unit_code = @employee&.[]("Unit")
+    unit = Unit.find_by(unit_id: unit_code)
+
+    department = Department.find_by(department_id: unit&.[]("department_id"))
+    division   = Division.find_by(division_id: department&.[]("division_id"))
+    agency     = Agency.find_by(agency_id: division&.[]("agency_id"))
+
+    @prefill_data = {
+      employee_id: @employee&.[]("EmployeeID"),
+      name: "#{@employee&.[]("First_Name")} #{@employee&.[]("Last_Name")}",
+      phone: @employee&.[]("Work_Phone"),
+      email: @employee&.[]("EE_Email"),
+      agency: agency&.agency_id,
+      division: division&.division_id,
+      department: department&.department_id,
+      unit: unit ? "#{unit.unit_id} - #{unit.long_name}" : nil
+    }
+
+    @agency_options = Agency.all.map { |a| [a.long_name, a.agency_id] }
+
+    @division_options = if agency
+      Division.where(agency_id: agency.agency_id).map { |d| [d.long_name, d.division_id] }
+    else
+      []
+    end
+
+    @department_options = if division
+      Department.where(division_id: division.division_id).map { |d| [d.long_name, d.department_id] }
+    else
+      []
+    end
+
+    @unit_options = if department
+      Unit.where(department_id: department.department_id).map { |u| ["#{u.unit_id} - #{u.short_name}", u.unit_id] }
+    else
+      []
+    end
+
+    @form_logo = "/assets/images/default-logo.svg"
+
+    @form_pages = [
+      {
+        title: "Employee Info",
+        fields: [
+          { name: "employee_id", label: "Employee ID", type: "text", required: true },
+          { name: "name", label: "Name", type: "text", required: true },
+          { name: "email", label: "Email", type: "text", required: true },
+          { name: "phone", label: "Phone", type: "text", required: true }
+        ]
+      },
+      {
+        title: "Agency Info"
+      },
+      {
+        title: "Transfer Request Details",
+        fields: [
+          { name: "work_location", label: "Work Location", type: "text", required: true },
+          { name: "current_assignment_date", label: "Current Assignment Date", type: "text", required: true },
+          { name: "desired_transfer_destination", label: "Desired Transfer Destination", type: "multi-select", options: ["East County", "West County", "Downtown", "Remote", "Other"] }
+        ]
+      }
+    ]
   end
 end
