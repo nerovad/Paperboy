f2873cc0b5a6ed870da57d8198b37ad2959f61ce
matthewdavoren <matthew.davoren@ventura.org>
2025-08-11 16:27:25 +0000

    style changes and RM75 alt to new db changes

--- app/controllers/loa_forms_controller.rb
+++ app/controllers/loa_forms_controller.rb
@@ -1,22 +1,53 @@
 class LoaFormsController < ApplicationController
-  def new
-    @loa_form = LoaForm.new
+def new
+  @loa_form = LoaForm.new
 
-    employee_id = session[:user]&.dig("employee_id")
-    @prefill_data = build_prefill_data(employee_id)
+  # Prefill basics (same helper you added)
+  employee_id = session[:user]&.dig("employee_id")
+  @prefill_data = build_prefill_data(employee_id)
 
-    @agency_options = Agency.all.map { |a| [a.long_name, a.agency_id] }
-    @division_options = []
-    @department_options = []
-    @unit_options = []
+  # Resolve the org chain based on the employee’s Unit -> Dept -> Division -> Agency
+  employee   = Employee.find_by(EmployeeID: employee_id)
+  unit_code  = employee&.[]("Unit")
+  unit       = Unit.find_by(unit_id: unit_code)
+  department = Department.find_by(department_id: unit&.department_id)
+  division   = Division.find_by(division_id: department&.division_id)
+  agency     = Agency.find_by(agency_id: division&.agency_id)
 
-    @form_pages = [
-      { title: "Employee Info" },
-      { title: "Leave Details" }
-    ]
+  # Build dropdown options (so the first render is populated)
+  @agency_options = Agency.all.map { |a| [a.long_name, a.agency_id] }
 
-    @form_logo = "/assets/images/default-logo.svg"
+  @division_options = if agency
+    Division.where(agency_id: agency.agency_id).map { |d| [d.long_name, d.division_id] }
+  else
+    []
   end
+
+  @department_options = if division
+    Department.where(division_id: division.division_id).map { |d| [d.long_name, d.department_id] }
+  else
+    []
+  end
+
+  @unit_options = if department
+    Unit.where(department_id: department.department_id).map { |u| ["#{u.unit_id} - #{u.short_name}", u.unit_id] }
+  else
+    []
+  end
+
+  # Ensure the selects have the right *IDs* selected on first render
+  @prefill_data[:agency]     = agency&.agency_id
+  @prefill_data[:division]   = division&.division_id
+  @prefill_data[:department] = department&.department_id
+  @prefill_data[:unit]       = unit&.unit_id
+
+  @form_pages = [
+    { title: "Employee Info" },
+    { title: "Leave Details" }
+  ]
+
+  @form_logo = "/assets/images/default-logo.svg"
+end
 
   def create
     @event = Event.create(
--- app/controllers/rm75_forms_controller.rb
+++ app/controllers/rm75_forms_controller.rb
@@ -1,35 +1,54 @@
+# app/controllers/rm75_forms_controller.rb
 class Rm75FormsController < ApplicationController
   def new
-    @event = Event.new(event_type: "rm75")
     @rm75_form = Rm75Form.new
+
+    employee_id = session[:user]&.dig("employee_id")
+    @prefill_data = build_prefill_data(employee_id)
+
+    # Org dropdowns (same pattern as Parking Lot / LOA)
+    employee   = Employee.find_by(EmployeeID: employee_id)
+    unit_code  = employee&.[]("Unit")
+    unit       = Unit.find_by(unit_id: unit_code)
+    department = Department.find_by(department_id: unit&.department_id)
+    division   = Division.find_by(division_id: department&.division_id)
+    agency     = Agency.find_by(agency_id: division&.agency_id)
+
+    @agency_options = Agency.all.map { |a| [a.long_name, a.agency_id] }
+    @division_options = agency ? Division.where(agency_id: agency.agency_id).map { |d| [d.long_name, d.division_id] } : []
+    @department_options = division ? Department.where(division_id: division.division_id).map { |d| [d.long_name, d.department_id] } : []
+    @unit_options = department ? Unit.where(department_id: department.department_id).map { |u| ["#{u.unit_id} - #{u.short_name}", u.unit_id] } : []
+
+    # Selected IDs for first render
+    @prefill_data[:agency]     = agency&.agency_id
+    @prefill_data[:division]   = division&.division_id
+    @prefill_data[:department] = department&.department_id
+    @prefill_data[:unit]       = unit&.unit_id
 
     @form_pages = [
       { title: "Injured Employee Info" },
-      { title: "Supervisor and Witness Info" },
+      { title: "Agency Info" },
       { title: "Incident Dates and Status" },
       { title: "Incident Description" },
       { title: "Medical Info" }
     ]
-
     @form_logo = "/assets/images/default-logo.svg"
   end
 
   def create
-    # First create the event
-    @event = Event.new(
+    # employee_id is NOT a column on rm75_forms; read it from top-level param
+    employee_id_for_event = params[:employee_id].presence || session[:user]&.dig("employee_id")
+
+    @event = Event.create!(
       event_type: "rm75",
-      employee_id: params[:rm75_form][:employee_id],
-      event_date: params[:rm75_form][:date_of_injury] || Time.zone.now
+      employee_id: employee_id_for_event,
+      event_date: params[:rm75_form][:date_of_injury].presence || Time.zone.now
     )
 
-    if @event.save
-      @rm75_form = Rm75Form.new(rm75_form_params.merge(event_id: @event.id))
+    @rm75_form = Rm75Form.new(rm75_form_params.merge(event_id: @event.id))
 
-      if @rm75_form.save
-        redirect_to root_path, notice: "RM-75 submitted!"
-      else
-        render :new
-      end
+    if @rm75_form.save
+      redirect_to root_path, notice: "RM-75 submitted!"
     else
       render :new
     end
@@ -38,15 +57,20 @@ class Rm75FormsController < ApplicationController
   private
 
   def rm75_form_params
+    # ONLY columns that exist on rm75_forms (per our migration)
     params.require(:rm75_form).permit(
-      :report_type, :bloodborne_pathogen, :job_title, :work_hours,
-      :hours_per_week, :supervisor_name, :supervisor_title, :supervisor_email,
-      :supervisor_phone, :form_completed_by, :date_of_injury, :time_of_injury,
-      :injury_description, :date_last_worked, :date_returned_to_work,
+      :report_type,
+      :bloodborne_pathogen,
+      :job_title, :work_hours, :hours_per_week,
+      :supervisor_name, :supervisor_title, :supervisor_phone, :supervisor_email,
+      :form_completed_by,
+      :date_of_injury, :time_of_injury,
+      :injury_description,
+      :date_last_worked, :date_returned_to_work,
       :location_of_event, :on_employers_premises, :department_of_exposure,
-      :equipment_in_use, :specific_activity, :injury_sequence, :physician_name,
-      :physician_address, :physician_phone, :hospital_name, :hospital_address,
-      :hospital_phone, :hospitalized_overnight
+      :equipment_in_use, :specific_activity, :injury_sequence,
+      :physician_name, :physician_address, :physician_phone,
+      :hospital_name, :hospital_address, :hospital_phone, :hospitalized_overnight
     )
   end
 end
