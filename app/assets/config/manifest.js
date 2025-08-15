4a7488a03207e8bd720806df2cc8f0fd604fbf05
matthewdavoren <matthew.davoren@ventura.org>
2025-08-15 00:57:23 +0000

    BUG FIX CSS cache fix with clobber and choices style change

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,6 +1,5 @@
 //= link_tree ../images
-//= link_directory ../stylesheets .scss
-//= link application.css
+//= link_directory ../stylesheets .css
 //= link application.js
 //= link_tree ../../javascript/controllers .js
 //= link form_navigation.js
