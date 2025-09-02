4e067cfdf08b19c7d80980c15b1d330286943b43
matthewdavoren <matthew.davoren@ventura.org>
2025-09-02 22:21:10 +0000

    BUG FIX - Unit id and long name not being applied when change of Agency Division or Dept

--- app/controllers/lookups_controller.rb
+++ app/controllers/lookups_controller.rb
@@ -1,45 +1,38 @@
 # app/controllers/lookups_controller.rb
 class LookupsController < ApplicationController
-    def divisions
+  def divisions
     @division_options = Division
       .where(agency_id: params[:agency])
       .order(:long_name)
       .pluck(:long_name, :division_id)
 
-    Rails.logger.info "[LOOKUPS] agency=#{params[:agency]} → #{@division_options.size} divisions"
-
     respond_to do |format|
       format.turbo_stream
       format.html { head :not_acceptable }
     end
   end
 
-    def departments
+  def departments
     @department_options = Department
       .where(division_id: params[:division])
       .order(:long_name)
       .pluck(:long_name, :department_id)
 
-    Rails.logger.info "[LOOKUPS] division=#{params[:division]} → #{@department_options.size} departments"
-
     respond_to do |format|
       format.turbo_stream
       format.html { head :not_acceptable }
     end
   end
-
 
   def units
     @unit_options = Unit
       .where(department_id: params[:department])
-      .order(:short_name)
-      .pluck(:short_name, :unit_id)
-
-    Rails.logger.info "[LOOKUPS] department=#{params[:department]} → #{@unit_options.size} units"
+      .order(:unit_id) # or :long_name, your choice
+      .map { |u| ["#{u.unit_id} - #{u.long_name}", u.unit_id] }  # <-- KEY CHANGE
 
     respond_to do |format|
       format.turbo_stream
       format.html { head :not_acceptable }
     end
   end
-end 
+end
