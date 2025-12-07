dda69f5b4e8642958cbcad3cfe22a088353a92de
matthewdavoren <matthew.davoren@ventura.org>
2025-12-07 20:30:51 +0000

    FIX - javascript register for sidebar search

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,10 +1,6 @@
 // app/javascript/controllers/index.js
 import { application } from "controllers/application"
 
-// Use importmap bare specifiers instead of relative ./ imports.
-// These are resolved via `pin_all_from "app/javascript/controllers", under: "controllers"`
-// in config/importmap.rb.
-
 import FormNavigationController from "controllers/form_navigation_controller"
 import LoginModalController from "controllers/login_modal_controller"
 import ProfileDropdownController from "controllers/profile_dropdown_controller"
@@ -17,6 +13,7 @@ import BillingModalController from "controllers/billing_modal_controller"
 import ApproveModalController from "controllers/approve_modal_controller"
 import ChoicesController from "controllers/choices_controller"
 import ConditionalFieldController from "controllers/conditional_field_controller"
+import SidebarSearchController from "controllers/sidebar_search_controller"
 
 // Register controllers with their data-controller names
 application.register("slideshow", SlideshowController)
@@ -31,3 +28,4 @@ application.register("billing-modal", BillingModalController)
 application.register("approve-modal", ApproveModalController)
 application.register("choices", ChoicesController)
 application.register("conditional-field-controller", ConditionalFieldController)
+application.register("sidebar-search", SidebarSearchController)
