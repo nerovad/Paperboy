869b324ceb89de46a9966f7aded548b8cbb5dff6
matthewdavoren <matthew.davoren@ventura.org>
2026-01-28 21:24:52 +0000

    Add helper method for time zone PST change

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -24,6 +24,11 @@ module ApplicationHelper
     return digits if d.length != 10
     "#{d[0,3]}-#{d[3,3]}-#{d[6,4]}"
   end
+
+  def format_pst(time, format: :short)
+    return nil unless time
+    l(time.in_time_zone("Pacific Time (US & Canada)"), format: format)
+  end
   
   def system_admin?
     return false unless session[:user_id]
