06c2081cbea555ab3ae49cc03bfeb5cc1e9ee3e6
Traap <gary.a.howard@mac.com>
2026-07-13 09:35:21 -0700

    Integrate DataRunner into master branch.
    - COA and DataRunner have their own left-side navigation bar.
    - FIX : COA slide show is not exempt for global content top padding.

--- app/controllers/coa/base_controller.rb
+++ app/controllers/coa/base_controller.rb
@@ -3,5 +3,53 @@
 module Coa
   class BaseController < ApplicationController
     before_action :require_system_admin
+
+    helper_method :coa_sidebar_resources, :coa_sidebar_collection_path
+
+    private
+
+    def coa_sidebar_resources
+      [
+        Coa::Agency,
+        Coa::Division,
+        Coa::Department,
+        Coa::Unit,
+        Coa::Activity,
+        Coa::Function,
+        Coa::Fund,
+        Coa::MajorProgram,
+        Coa::Object,
+        Coa::Phase,
+        Coa::Program,
+        Coa::RevenueSource,
+        Coa::ObjectInference,
+        Coa::SubUnit,
+        Coa::Task
+      ]
+    end
+
+    def coa_sidebar_collection_path(model_class)
+      public_send("coa_#{coa_route_collection_name(model_class)}_path")
+    end
+
+    def coa_route_collection_name(model_class)
+      {
+        "Coa::Agency" => "agencies",
+        "Coa::Activity" => "activities",
+        "Coa::Department" => "departments",
+        "Coa::Division" => "divisions",
+        "Coa::Function" => "functions",
+        "Coa::Fund" => "funds",
+        "Coa::MajorProgram" => "major_programs",
+        "Coa::Object" => "objects",
+        "Coa::ObjectInference" => "object_inferences",
+        "Coa::Phase" => "phases",
+        "Coa::Program" => "programs",
+        "Coa::RevenueSource" => "revenue_sources",
+        "Coa::SubUnit" => "sub_units",
+        "Coa::Task" => "tasks",
+        "Coa::Unit" => "units"
+      }.fetch(model_class.name)
+    end
   end
 end
