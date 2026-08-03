db217cb433455781dc3e9463f99f5bddda334069
Traap <gary.a.howard@mac.com>
2026-08-03 13:57:00 -0700

    Fix document automation dsl.

--- config/data_runner/dsl/document_automation.rb
+++ config/data_runner/dsl/document_automation.rb
@@ -15,7 +15,7 @@
       name: 'billing'
     },
     source: {
-      location: '/mnt/i/BUSINESS_SUPPORT/DataRunner/00_Inbox/document_automation.csv',
+      location: '/mnt/i/BUSINESS_SUPPORT/DataRunner/00_Inbox/document_automation.xlsx',
       local: 'document_automation.xlsx',
       format: :xlsx,
       strategy: :copy
