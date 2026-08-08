217469ae647347c1ccfe0ec856eea9ce3ed0464a
Traap <gary.a.howard@mac.com>
2026-08-07 23:01:09 -0700

    Enforce script naming convention.
    - dsl/oversized.rb
    - script/ruby/data_runner/download/oversized.rb
    - scritp/ruby/data_runner/inject/oversized.rb

--- config/data_runner/dsl/oversized.rb
+++ config/data_runner/dsl/oversized.rb
@@ -50,7 +50,7 @@
         inject: {
           mode: :truncate_insert,
           post_script: {
-            path: 'script/ruby/data_runner/inject/oversized_post_inject.rb'
+            path: 'script/ruby/data_runner/inject/oversized.rb'
           }
         }
       }
