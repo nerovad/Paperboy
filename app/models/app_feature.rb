453620318441bfb43ea45e46262ca69ac8a30180
Traap <gary.a.howard@mac.com>
2026-08-21 11:18:00 -0700

    Change Mail.dat labels to Post Production

--- app/models/app_feature.rb
+++ app/models/app_feature.rb
@@ -42,7 +42,7 @@ class AppFeature
       { key: 'archive_reports',     label: 'Archive Billing Reports' }
     ],
     'p2m' => [
-      { key: 'stage_data', label: 'Mail.dat uploaded to USPS' },
+      { key: 'stage_data', label: 'Post Production' },
       { key: 'data_refresh', label: 'Refresh Data' }
     ],
     # Keys match Coa::BaseController#coa_route_collection_name, which is also
