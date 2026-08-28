d657b7441fa0daa378dec9ef06890aed78576a53
Traap <gary.a.howard@mac.com>
2026-08-28 13:41:02 -0700

    Group form templates in the admin sidebar
    Replace the Manage Forms link with an expandable action group.
    List Create Form first, followed by alphabetized template edit links.

--- db/gsabss/truncate_p2m_oms_upload_data.sql
+++ db/gsabss/truncate_p2m_oms_upload_data.sql
@@ -1,11 +1,13 @@
--- select count(*) from GSABSS.dbo.companions;
--- select count(*) from GSABSS.dbo.daily_presorts;
--- select count(*) from GSABSS.dbo.move_results;
+select 
+   (select count(*) from GSABSS.dbo.companions) as "#Companions"
+  ,(select count(*) from GSABSS.dbo.daily_presorts) as "#Daily Presorts"
+  ,(select count(*) from GSABSS.dbo.move_results) as "Move Results"
+;
 
-truncate table GSABSS.dbo.companions;
-truncate table GSABSS.dbo.daily_presorts;
-truncate table GSABSS.dbo.move_results;
-truncate table GSABSS.dbo.p2m_oms_upload_files;
-truncate table GSABSS.dbo.p2m_oms_upload_findings;
+-- truncate table GSABSS.dbo.companions;
+-- truncate table GSABSS.dbo.daily_presorts;
+-- truncate table GSABSS.dbo.move_results;
+-- truncate table GSABSS.dbo.p2m_oms_upload_files;
+-- truncate table GSABSS.dbo.p2m_oms_upload_findings;
 
-delete from GSABSS.dbo.p2m_oms_uploads where oms_number is not null;
+-- delete from GSABSS.dbo.p2m_oms_uploads where oms_number is not null;
---
+++ test/controllers/admin_tools/sidebar_test.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class AdminToolsSidebarTest < ActionController::TestCase
+  tests FormTemplatesController
+
+  setup do
+    session[:user] = { 'employee_id' => 1, 'email' => 'employee@example.com',
+                       'first_name' => 'Test', 'last_name' => 'User' }
+    @controller.define_singleton_method(:current_user_group_names) { Set['system_admins'] }
+    @controller.define_singleton_method(:inbox_count) { 0 }
+  end
+
+  test 'groups create and form template actions under Manage Forms' do
+    Forms::Template.create!(name: 'Zulu Form', class_name: 'ZuluForm', page_count: 2,
+                            submission_type: 'database')
+    template = Forms::Template.create!(name: 'Alpha Form', class_name: 'AlphaForm', page_count: 2,
+                                       submission_type: 'database')
+
+    get :edit, params: { id: template.id }
+
+    assert_response :success
+    assert_select '.admin-tools-sidebar details.nav-group[open]' do
+      assert_select 'summary', text: /Manage Forms/
+      assert_select 'a.nav-link', text: ['Create Form', 'Alpha Form', 'Zulu Form']
+      assert_select "a.nav-link.active[href='#{edit_form_template_path(template)}']", text: 'Alpha Form'
+    end
+    assert_select "a.nav-link[href='#{form_templates_path(create: true)}']", text: 'Create Form'
+  end
+end
