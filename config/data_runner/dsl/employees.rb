04fb6ef016e223137f81876d0fc1fb664cece575
Traap <gary.a.howard@mac.com>
2026-08-28 20:52:53 -0700

    wip: replicate employees

--- config/data_runner/dsl/employees.rb
+++ config/data_runner/dsl/employees.rb
@@ -1,33 +1,25 @@
 # frozen_string_literal: true
 
 [
-  'Employees',
+  "Employees",
   {
     steps: {
-      manual: {
-        enabled: false,
-        steps: Workflow::MANUAL_STEPS
-      },
-      scheduled: {
-        enabled: false,
-        frequency: :daily,
-        steps: Workflow::SCHEDULED_STEPS
-      }
+      manual: { enabled: true, steps: Workflow::MANUAL_STEPS },
+      scheduled: { enabled: true, frequency: :daily, steps: Workflow::SCHEDULED_STEPS }
     },
     group: {
       name: 'human_resources'
     },
     source: {
-      location: '/mnt/i/BUSINESS_SUPPORT/DataRunner/00_Inbox/employees.xlsx',
-      local: 'employees.xlsx',
-      format: :xlsx,
-      strategy: :copy
+      host: "GSASQL16",
+      database: "GSABSS",
+      schema: "dbo",
+      table: "employees",
+      local: "employees.csv",
+      format: :csv,
+      strategy: :replicate
     },
-    to_csv: {
-      sheet: 0,
-      header_row: 1,
-      data_row: 2
-    },
+
     header: [
 
       ['id',                    'id',                    'int',          'NOT NULL', nil],
@@ -46,20 +38,17 @@
       ['supervisor_id',         'supervisor_id',         'int',          'NULL',     nil],
       ['supervisor_last_name',  'supervisor_last_name',  'nvarchar(50)', 'NULL',     nil],
       ['supervisor_first_name', 'supervisor_first_name', 'nvarchar(50)', 'NULL',     nil],
-      ['person_email',          'person_email',          'nvarchar(50)', 'NULL',     nil],
+      ['person_email',          nil,                     'nvarchar(50)', 'NULL',     nil],
       ['email',                 'email',                 'nvarchar(50)', 'NULL',     nil],
       [nil,                     'active',                'bit',          'NOT NULL', 1],
-
     ],
     database_connections: [
       {
-        host: 'GSASQL16',
-        database: 'GSABSS',
-        schema: 'dbo',
-        table: 'employees',
-        inject: {
-          mode: :truncate_insert
-        }
+        host: "GSASQL16",
+        database: "GSARecords",
+        schema: "dbo",
+        table: "employees",
+        inject: { mode: :truncate_insert }
       }
     ]
   }
